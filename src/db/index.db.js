fd1ccef7dd6079c689eb7bacc4520adc49ce1981
jawad ilyas <mughaljawad12@gmail.com>
2024-09-14 19:26:32 +0500

    changing into backend db connection

--- src/db/index.db.js
+++ src/db/index.db.js
@@ -4,7 +4,11 @@ import mongoose from "mongoose";
 const connectDb = () => {
 
     try {
-        const connection = mongoose.connect("mongodb+srv://jawaddb:jawaddb@cluster0.6erxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const connection = mongoose.connect("mongodb+srv://jawaddb:jawaddb@cluster0.6erxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Adjust the timeout duration as needed
+        });
         return connection;
     } catch (error) {
         console.log("index db :: connect db :: error ", error)
